7145896c3b1fa09de2b858d7221aba65c5f90321
Harshitha <harshithadarshan95@gmail.com>
2023-02-04 13:33:26 +0530

    Autopush

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,5 +3,5 @@ def sonarChecks(COMPONENT) {
        sh  "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
        sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
        sh  "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
-       sh  "echo Code Quality Checks Completed."
+       sh  "echo Code Quality Checks Compleed."
     }
