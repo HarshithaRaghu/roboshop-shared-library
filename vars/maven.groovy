c3f243ac6110625f43594f1c6b8d42c67c456f21
Harshitha <harshithadarshan95@gmail.com>
2023-02-06 11:54:55 +0530

    Autopush

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,13 +5,26 @@ def lintChecks(COMPONENT) {
         sh "echo lint checks completed for ${COMPONENT}.....!!!!!"
 }
 
+def call() {
+     node {
+        env.APP = "maven"
+        lintChecks()
+        sh "mvn clean compile"
+        env.ARGS="-Dsonar.java.binaries=target/"
+        common.sonarChecks() 
+        common.testCases() 
+        common.artifacts()
+
+
 def call(COMPONENT)                                              // call is the default function that's called by default.
 {
     pipeline {
         agent any 
         environment {
             SONAR = credentials('SONAR')
+            NEXUS = credentials('NEXUS')
             SONAR_URL = "172.31.12.196"
+            NEXUS_URL = "172.31.7.27"
         }
         
         stages {                                        // Start of Stages
@@ -55,23 +68,39 @@ def call(COMPONENT)                                              // call is the 
                     }
                 }
 
-        stage('Downloading the dependencies') {
+        stage('Artifact Validation On Nexus') {
+                 when { 
+                     expression { env.TAG_NAME != null } 
+                     }
+                 steps {
+                     sh "echo checking whether artifact exists of not. If it doesnt exist then only proceed with Preparation and Upload"
+                     script {
+                         env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
+                     }
+                 }
+             }
+        stage('Preparing the artifact') {
                 when { 
-                    expression { env.TAG_NAME != null } 
-                }
-                steps {
-                    sh "mvn clean package"
-                }
-            }
-       
+                     expression { env.TAG_NAME != null } 
+                     expression { env.UPLOAD_STATUS == "" }
+                     }
+                 steps {
+                     sh "mvn clean package"
+                     sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
+                     sh "zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar"
+                     sh "ls -ltr"
+                 }
+             }
+
         stage('Uploading the artifact') {
-                when { 
-                    expression { env.TAG_NAME != null } 
-                    }
-                steps {
-                    sh "echo uploading artifact to nexus"
-                }
-            }
+                 when { 
+                     expression { env.TAG_NAME != null } 
+                     expression { env.UPLOAD_STATUS == "" }
+                     }
+                 steps {
+                     sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                 }
+             }
         
         
         
