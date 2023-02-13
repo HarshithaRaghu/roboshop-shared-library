b419d0e12da98353ba97b878e58abca31a265ce8
Harshitha <harshithadarshan95@gmail.com>
2023-02-13 17:46:07 +0530

    Autopush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@ def lintChecks(COMPONENT) {
         sh "echo Installing JSLINT"
         sh "npm install jslint"
         sh "ls -ltr node_modules/jslint/bin/"
-        // sh "./node_modules/jslint/bin/jslint.js server.js"
+        sh "./node_modules/jslint/bin/jslint.js server.js"
         sh "echo lint checks completed for ${COMPONENT}.....!!!!!"
 }
 
