35dfae1ac7738d1aa5c286431b15d02454bf8894
Harshitha <harshithadarshan95@gmail.com>
2023-02-27 11:30:51 +0530

    Autopush

---
+++ vars/terraform.groovy
@@ -0,0 +1,76 @@
+def call() {
+    
+    if (!env.TF_DIRECTORY) {
+            env.TF_DIRECTORY = "./"
+    }
+
+    properties(
+        [
+            parameters([
+                    choice(name: 'ENV', choices: 'dev\nprod' , description: 'Chose an environment' ),
+                    choice(name: 'ACTION', choices: 'apply\ndestroy' , description: 'Chose action to be apply or destroy')
+            ])   
+        ])
+    node {
+        ansiColor('xterm') {
+        sh "rm -rf *"
+        git branch: 'main', url: "https://github.com/HarshithaRaghu/${REPONAME}.git"
+        
+        stage('Terraform Init') {
+                sh ''' 
+                       cd ${TF_DIRECTORY}
+                       terrafile -f env-${ENV}/Terrafile
+                       terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars
+                   '''            
+            }
+
+        stage('Terraform Plan') {
+                sh ''' 
+                       cd ${TF_DIRECTORY}
+                       terraform plan -var-file=env-${ENV}/${ENV}.tfvars
+                   '''            
+            }
+
+        stage('Terraform Action') {
+                sh ''' 
+                        cd ${TF_DIRECTORY}
+                        terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve
+                   '''            
+            }            
+        }
+    }
+}
+
+// pipeline {
+//     agent any 
+
+//         parameters {
+//              choice(name: 'ENV', choices: ['dev', 'prod'], description: 'Chose an environment')
+//              choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Chose action to be apply or destroy')
+//         }
+        
+//         options {
+//             ansiColor('xterm')
+//         }
+
+//     stages {
+//         stage('Terraform Init') {
+//             steps {
+//                 sh "terrafile -f env-${ENV}/Terrafile"
+//                 sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
+//             }
+//         }
+
+//         stage('Terraform Plan') {
+//             steps {
+//                 sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+//             }
+//         }
+
+//         stage('Terraform Action') {
+//             steps {
+//                 sh "terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve"
+//             }
+//         }
+//     }
+// }
