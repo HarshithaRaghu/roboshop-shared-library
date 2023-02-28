716c60ec811c14aa7f1eec4d20d6c5256c88c0d0
Harshitha <harshithadarshan95@gmail.com>
2023-02-28 10:39:08 +0530

    Autopush

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,13 +8,14 @@ def call() {
         [
             parameters([
                     choice(name: 'ENV', choices: 'dev\nprod' , description: 'Chose an environment' ),
-                    choice(name: 'ACTION', choices: 'apply\ndestroy' , description: 'Chose action to be apply or destroy')
+                    choice(name: 'ACTION', choices: 'apply\ndestroy' , description: 'Chose action to be apply or destroy'),
+                    string(name: 'APP_VERSION', choices: 'APP_VERSION' , description: 'Chose the app version to be deployed IGNORE THIS VALUE FOR INFRA')
             ])   
         ])
     node {
         ansiColor('xterm') {
         sh "rm -rf *"
-        git branch: 'main', url: "https://github.com/HarshithaRaghu/${REPONAME}.git"
+        git branch: 'main', url: "https://github.com/b52-clouddevops/${REPONAME}.git"
         
         stage('Terraform Init') {
                 sh ''' 
@@ -27,14 +28,14 @@ def call() {
         stage('Terraform Plan') {
                 sh ''' 
                        cd ${TF_DIRECTORY}
-                       terraform plan -var-file=env-${ENV}/${ENV}.tfvars
+                       terraform plan -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
                    '''            
             }
 
         stage('Terraform Action') {
                 sh ''' 
                         cd ${TF_DIRECTORY}
-                        terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve
+                        terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars  -var APP_VERSION=${APP_VERSION} -auto-approve
                    '''            
             }            
         }
